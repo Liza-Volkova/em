4051ae8f5923983905e05aaf977e5d0f6dfa86eb
HanZamai <lizkinmir93@gmail.com>
2025-11-01 00:40:34 +0400

    lesson8

--- lesson8/Task1.js
+++ lesson8/Task1.js
@@ -11,5 +11,19 @@ const Request2 = () => new Promise((resolve, reject) => {
 });
 
 // через async/await
+(async () => {
+	const result1 = await Request1();
+	console.log(result1);
+	const result2 = await Request2();
+	console.log(result2);
+})();
 
 // Через .then
+Request1()
+    .then(result1 => {
+        console.log(result1);
+        return Request2();
+    })
+    .then(result2 => {
+        console.log(result2);
+    });
